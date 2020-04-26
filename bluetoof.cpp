2612efbab10bd46d6a756c0121c0ccfc7a87be2a
natelovalo <natelovallo@gmail.com>
2020-04-26 16:48:25 -0600

    I make class. classy

--- bluetoof.cpp
+++ bluetoof.cpp
@@ -1,97 +1,20 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
-#include <bluetooth/rfcomm.h>
-#include <bluetooth/sdp.h>
-#include <bluetooth/sdp_lib.h>
-
 
 #include <glib.h>
 #include <gio/gio.h>
 
-
-
-static void hark_a_device(GDBusConnection* sig,
-	const gchar* sender_name,
-	const gchar* object_path,
-	const gchar* interface,
-	const gchar* signal_name,
-	GVariant* parameters,
-	gpointer user_data)
-{
-	printf("HARK\n");
-}
-
-
+#include "bluetoof.h"
 
 int main(int argc, char* args[])
 {
 	printf("bluetoof\n");
+
+	BluetoothManager bMan;
+
+	bMan.Discover();
  
-	GDBusConnection* connection = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL);
-	if (connection == NULL)
-	{
-		g_print("FFFFFFF\n");
-		return 1;
-	}
 
-	GMainLoop* loop = g_main_loop_new(NULL, FALSE);;
-	GVariant* result;
-	GError* error = NULL;
-	guint iface_added;
-
-	result = g_dbus_connection_call_sync(connection,
-		"org.bluez",
-		"/org/bluez/hci0",
-		"org.bluez.Adapter1",
-		"StartDiscovery",
-		NULL,
-		NULL,
-		G_DBUS_CALL_FLAGS_NONE,
-		-1,
-		NULL,
-		&error);
-
-	if (error != NULL)
-	{
-		printf("OH NO\n");
-	}
-
-
-	iface_added = g_dbus_connection_signal_subscribe(
-		connection,
-		"org.bluez",
-		"org.freedesktop.DBus.ObjectManager",
-		"InterfacesAdded",
-		NULL,
-		NULL,
-		G_DBUS_SIGNAL_FLAGS_NONE,
-		hark_a_device,
-		loop,
-		NULL);
-
-
-
-	g_main_loop_run(loop);
-	result = g_dbus_connection_call_sync(connection,
-		"org.bluez",
-		"/org/bluez/hci0",
-		"org.bluez.Adapter1",
-		"StopDiscovery",
-		NULL,
-		NULL,
-		G_DBUS_CALL_FLAGS_NONE,
-		-1,
-		NULL,
-		&error);
-
-	g_variant_unref(result);
-
-	g_object_unref(connection);
-   
 	return 0;
 }
---
+++ include/bluetoof.h
@@ -0,0 +1,184 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <glib.h>
+#include <gio/gio.h>
+
+#include <assert.h>
+
+#include <vector>
+#include <string>
+
+///////////////////////////////////////////////////////////////////////////////
+// Manager for interacting with bluez using an OO class
+//
+// This borrows heavily from:
+// https://www.linumiz.com/bluetooth-adapter-scan-for-new-devices-using-startdiscovery/
+// with my own OO sauce on top
+class BluetoothManager
+{
+public:
+	//////////////////////////////////////////////////////////////////////////////
+	BluetoothManager()
+	{
+		mConnection = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL);
+		if (mConnection == NULL)
+		{
+			g_print("FFFFFFF\n");
+			assert(false);
+		}
+
+		//mLoop = g_main_loop_new(NULL, FALSE);
+	}
+
+	//////////////////////////////////////////////////////////////////////////////
+	void Discover()
+	{
+		guint interfaceAdded = g_dbus_connection_signal_subscribe(
+			mConnection,
+			"org.bluez",
+			"org.freedesktop.DBus.ObjectManager",
+			"InterfacesAdded",
+			NULL,
+			NULL,
+			G_DBUS_SIGNAL_FLAGS_NONE,
+			DeviceAppearedCb,
+			this,
+			NULL);
+
+		unsigned int seconds = 0;
+		InvokeG("StartDiscovery");
+
+		for (int seconds = 0; seconds < 10; seconds++)
+		{ 
+			g_main_context_iteration(NULL, FALSE);
+			g_usleep(1000000);
+			g_thread_yield();
+		}
+
+		g_dbus_connection_signal_unsubscribe(mConnection, interfaceAdded);
+		InvokeG("StopDiscovery");
+		printf("Found %u devices! in %u seconds\n", mDevices.size(), seconds);
+
+	}
+
+	//////////////////////////////////////////////////////////////////////////////
+	~BluetoothManager()
+	{
+		g_object_unref(mConnection);
+	}
+
+
+
+private:
+
+	//////////////////////////////////////////////////////////////////////////////
+	bool InvokeG(const char* method)
+	{
+		bool retval = true;
+		GError* error = NULL;
+		g_dbus_connection_call_sync(mConnection,
+			"org.bluez",
+			"/org/bluez/hci0",
+			"org.bluez.Adapter1",
+			method,
+			NULL,
+			NULL,
+			G_DBUS_CALL_FLAGS_NONE,
+			-1,
+			NULL,
+			&error);
+
+		if (error != NULL)
+		{
+			printf("OH NO\n");
+			retval = false;
+		}
+		return retval;
+	}
+
+	//////////////////////////////////////////////////////////////////////////////
+	static void PropertyGetter(const gchar* key, GVariant* value)
+	{
+		const gchar* type = g_variant_get_type_string(value);
+
+		g_print("\t%s : ", key);
+		switch (*type) 
+		{
+			case 'o':
+			case 's':
+				g_print("%s\n", g_variant_get_string(value, NULL));
+				break;
+			case 'b':
+				g_print("%d\n", g_variant_get_boolean(value));
+				break;
+			case 'u':
+				g_print("%d\n", g_variant_get_uint32(value));
+				break;
+			case 'a':
+				// TODO Handling only 'as', but not array of dicts 
+				if (g_strcmp0(type, "as"))
+					break;
+				g_print("\n");
+				const gchar* uuid;
+				GVariantIter i;
+				g_variant_iter_init(&i, value);
+				while (g_variant_iter_next(&i, "s", &uuid))
+				{
+					g_print("\t\t%s\n", uuid);
+				}
+				break;
+			default:
+				g_print("Other\n");
+				break;
+		}
+	}
+
+	//////////////////////////////////////////////////////////////////////////////
+	static void DeviceAppearedCb(
+		GDBusConnection* sig,
+		const gchar* sender,
+		const gchar* path,
+		const gchar* interface,
+		const gchar* signal,
+		GVariant* param,
+		gpointer userData)
+	{
+		GVariantIter* interfaces;
+		const char* object;
+		const gchar* interface_name;
+		GVariant* properties;
+
+		assert(userData != NULL);
+		BluetoothManager* me = static_cast<BluetoothManager*>(userData);
+
+		g_variant_get(param, "(&oa{sa{sv}})", &object, &interfaces);
+		while (g_variant_iter_next(interfaces, "{&s@a{sv}}", &interface_name, &properties)) 
+		{
+			if (g_strstr_len(g_ascii_strdown(interface_name, -1), -1, "device")) 
+			{
+				g_print("[ %s ]\n", object);
+				const gchar* propertyName;
+				GVariantIter i;
+				GVariant* propertyValue;
+				g_variant_iter_init(&i, properties);
+				while (g_variant_iter_next(&i, "{&sv}", &propertyName, &propertyValue))
+				{ 
+					PropertyGetter(propertyName, propertyValue);
+				}
+				g_variant_unref(propertyValue);
+
+
+				me->mDevices.push_back(std::string(object));
+			}
+
+			g_variant_unref(properties);
+		}
+		return;
+	}
+
+	GDBusConnection* mConnection = nullptr;
+	//GMainLoop* mLoop = ;
+	std::vector <std::string> mDevices;
+};
